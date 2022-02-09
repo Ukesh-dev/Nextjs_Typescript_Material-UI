dfca78c409ffed23f590ce43f9ea27cf6beef99f
Ukesh Shrestha <uks56shr567@gmail.com>
2022-02-09 08:46:41 +0545

    Next Image Tag Refactored

--- components/products/GridItem.tsx
+++ components/products/GridItem.tsx
@@ -9,8 +9,10 @@ import {
   Typography,
   CardActions,
 } from "@mui/material";
+import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import { ProductType } from "../../interfaces/dataType";
+import Image from "next/image";
 // import useStyles from "../../utils/styles";
 const ButtonStyled = styled(Button)`
   &:hover {
@@ -24,18 +26,34 @@ const GridItem = ({ products }: { products: ProductType }) => {
     <Grid
       item
       xs={12}
-      sm={4}
-      md={3}
+      sm={6}
+      md={4}
+      lg={3}
       // className={classes.navbar}
     >
-      <Card variant="outlined">
+      <Card
+        variant="outlined"
+        css={css`
+          background-color: var(--navbar-bg);
+        `}
+      >
         <Link href={`/product/${products.slug}`} passHref>
           <CardActionArea>
-            <CardMedia
+            {/* <CardMedia
               component="img"
-              image={products.image}
               title={products.name}
-            ></CardMedia>
+              src={products.image}
+            > */}
+            <Image
+              width="400px"
+              height="400px"
+              alt={products.name}
+              src={products.image}
+              className="custom-img"
+              objectFit="contain"
+              priority
+            ></Image>
+            {/* </CardMedia> */}
             <CardContent>
               <Typography>{products.name}</Typography>
             </CardContent>
